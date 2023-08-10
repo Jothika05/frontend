61e4d6543d7858ed5f24ac398f5fc09765b332cd
Aruljothi S <132689076+Jothika05@users.noreply.github.com>
2023-08-10 10:46:00 +0530

    Add files via upload

---
+++ Typescript/build/hello.js
@@ -0,0 +1,7 @@
+"use strict";
+// Declaring primitives
+// In JS : let x1=10;
+// In TS
+let x = 10;
+// console.log("x1=" + (x1 + 25));
+console.log(`x= ${x + 25}`);
---
+++ Typescript/src/hello.ts
@@ -0,0 +1,9 @@
+// Declaring primitives
+
+// In JS : let x1=10;
+
+// In TS
+let x:number = 10;
+
+// console.log("x1=" + (x1 + 25));
+console.log(`x= ${x + 25}`);
